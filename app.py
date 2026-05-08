2a46b1e1573f69015d4c6cebfdab7fcfa425cc3c
daniloperoj-commits <daniloperoj@gmail.com>
2026-05-08 22:00:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1140,7 +1140,7 @@ if modo_debug:
 st.markdown("------")
 
 st.markdown(
-    "<p style='text-align:center; font-size:12px; color:gray;'>v2.0-BETA</p>",
+    "<p style='text-align:center; font-size:12px; color:gray;'>v2.0_BETA</p>",
     unsafe_allow_html=True
 )
 
